5be6271a16739cbcf22a4c8f3bd910b96a6e09d5
Bismarck <erikblunk42@gmail.com>
2025-01-23 11:53:45 +0100

    implement json decoding

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-from modules.bot import Bot 
+from modules.bot import Bot
 
+import string
+import os
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+Bot.check_tags(
+    'Hallo, ich hätte gern ein Test1, oder Test2, vielen dank im vorraus :)')
--- modules/bot.py
+++ modules/bot.py
@@ -1,3 +1,7 @@
+import json
+import string
+
+
 class Bot():
 
     def input(user_text):
@@ -6,13 +10,36 @@ class Bot():
         Bot.answer(tags)
         Bot.log_tags(tags)
 
-
     def check_tags(question):
-        pass
+
+        translator = str.maketrans('', '', string.punctuation) # translation key
+
+        q_striped = question.translate(translator) # 
+        q_list = q_striped.lower().split()
+
+        with open('modules/functionals/tags.json') as json_file:
+            json_tag_data = json.load(json_file)
+
+            # {
+            # "tags": [
+            #    {
+            #       "tag": [
+            #           "key", "words"
+            #       ]
+            #    }
+            # }
+
+            q_tags = []
+
+            for tag in json_tag_data["tags"]:
+                for values in tag.values():
+                    for word in q_list:
+                        if word in values:
+                            # tag.keys returns dict_keys object --> conversion to string with list
+                            q_tags.append(list(tag.keys())[0])
 
     def answer(tags):
         pass
 
     def log_tags():
         pass
-
