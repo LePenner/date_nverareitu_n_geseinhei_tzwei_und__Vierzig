6ae185deb7103d347a55563fa5815add479534f5
Bismarck <erikblunk42@gmail.com>
2025-01-24 12:06:42 +0100

    remove duplicate tags from return

--- modules/bot.py
+++ modules/bot.py
@@ -4,20 +4,20 @@ import string
 
 class Bot():
 
-    def input(user_text):
+    def input(user_question, api):
 
-        tags = Bot.check_tags(user_text)
-        Bot.answer(tags)
+        tags = Bot.check_tags(user_question)
+        Bot.answer(tags, api)
         Bot.log_tags(tags)
 
     def check_tags(question):
 
         translator = str.maketrans('', '', string.punctuation) # translation key
 
-        q_striped = question.translate(translator) # 
+        q_striped = question.translate(translator) # remove punktuation from string
         q_list = q_striped.lower().split()
 
-        with open('modules/functionals/tags.json') as json_file:
+        with open('modules/functionals/tags.json', 'r') as json_file:
             json_tag_data = json.load(json_file)
 
             # {
@@ -31,12 +31,25 @@ class Bot():
 
             q_tags = []
 
+            # excess keywords from json file and get tags of matching words from user question
             for tag in json_tag_data["tags"]:
                 for values in tag.values():
                     for word in q_list:
                         if word in values:
                             # tag.keys returns dict_keys object --> conversion to string with list
                             q_tags.append(list(tag.keys())[0])
+
+        # remove duplicates
+        t_list = []
+        	
+        for element in q_tags:
+            if element not in t_list:
+                t_list.append(element)
+
+        q_tags = t_list.copy()
+    
+
+        return q_tags
 
     def answer(tags):
         pass
