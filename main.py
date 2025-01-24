a18890b7bd0e1628d4440f8c8fbe523b601ec100
Bismarck <erikblunk42@gmail.com>
2025-01-24 12:29:21 +0100

    push

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 from modules.bot import Bot
 
-import string
-import os
-
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
-
-Bot.check_tags(
-    'Hallo, ich hätte gern ein Test1, oder Test2, vielen dank im vorraus :)')
+print(Bot.check_tags(
+    "My cleaning robot doesnt work, what can i do?"))
