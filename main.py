5c4d83db9016b2aed38ceff3fa8af9c883c5dae5
Kira <susanneblunk84@gmail.com>
2025-01-28 10:24:28 +0100

    Ticket class

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ from modules.functionals.sending_messages import send_mail
 import os
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+send_mail('thilo.butt@gmail.com', 'Test Header',
+          'Jetzt wird wieder in die Hände gespuckt, wir steigern das Bruttosozialprodukt!')
---
+++ modules/ticket.py
@@ -0,0 +1,11 @@
+class Ticket():
+    def __init__(self, question = str, product_tags = list, problem_tags = list, level = int, extra_bin = str):
+        super().__init__
+        self.question = question
+        self.product_tags = product_tags
+        self.problem_tags = problem_tags
+        self.level = level
+        self.extra_bin = extra_bin
+
+    def escalate():
+        pass
