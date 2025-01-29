cab53c81fcb1b27e642c590c6468b31f4b992320
Kira <susanneblunk84@gmail.com>
2025-01-29 02:47:43 +0100

    cleanup

--- modules/functionals/sending_messages.py
+++ modules/functionals/sending_messages.py
@@ -2,11 +2,11 @@ import os
 import base64
 
 from email.message import EmailMessage
-from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+
+from modules.console import Console
 
 # If modifying these scopes, delete the file token.json.
 # https://mail.google.com gives perms do delete, read and write mails
@@ -26,21 +26,7 @@ def send_mail(adress, header, content):
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists(token_json):
-        creds = Credentials.from_authorized_user_file(token_json, SCOPES)
-
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                credentials_json, SCOPES
-            )
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open(token_json, "w") as token:
-            token.write(creds.to_json())
+    creds = Credentials.from_authorized_user_file(token_json, SCOPES)
 
     try:
         service = build("gmail", "v1", credentials=creds)
@@ -64,8 +50,8 @@ def send_mail(adress, header, content):
             .send(userId="me", body=create_message)
             .execute()
         )
-        print(f'Message sent. Id: {send_message["id"]}')
+        Console.status(f'Message sent. Id: {send_message["id"]}')
     except HttpError as error:
-        print(f"An error occurred: {error}")
+        Console.status(f"An error occurred: {error}")
         send_message = None
     return send_message
