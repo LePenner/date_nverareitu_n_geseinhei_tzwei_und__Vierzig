734580914d51025017b6f1f3ef737f2c90709ad5
Kira <susanneblunk84@gmail.com>
2025-02-02 16:21:05 +0100

    redid broken db stuff - no legasy tags

--- modules/ticket.py
+++ modules/ticket.py
@@ -1,6 +1,7 @@
 import sqlite3
 from modules.console import Console
 
+
 class Ticket_db():
     def __init__(self):
         super().__init__()
@@ -15,8 +16,8 @@ class Ticket_db():
                 customer_mail VARCHAR(255),
                 complaint TEXT,
                 history TEXT,
-                tags_ai TEXT,
-                tags_legacy TEXT,
+                tags_ai BLOB,
+                tags_legacy BLOB,
                 level INTEGER,
                 extra_bin TEXT
             )""")
@@ -35,48 +36,38 @@ class Ticket_db():
         Console.log("received all data")
         thread_id = data["thread_id"]
         name = data["name"]
+
+        # no legacy tags?
+
         tags_legacy = []
         history_str = AIResponse
-        tags_legacy_str = ','.join(tags_legacy)
+        tags_legacy_str = tags_legacy
         extra_bin = ''
         level = 2
         Console.log("refactored all data")
 
-        """ai_details = {
-            "Problems": [
-                {
-                    "description": "Battery explosion",
-                    "tags": ["physical problem", "battery issue"],
-                    "Continue": "employe"
-                }
-            ]
-        }"""
+        """{'tags': {'product': 'Gardenbeetle', 'problem': ['Will not turn on after submersion in water']}, 'continue': 'employee'}"""
         Console.log("l52")
         try:
-            problems = ai_details["Problems"]
-            Console.log("l54")
-            for problem in problems:
-                Console.log("l56")
-                tags_ai = problem["tags"]
-                Console.log("l58")
-                tags_ai_str = ','.join(tags_ai)
-                Console.log("l60")
-                sql_data = (ticket_id, thread_id, name, customer_mail, complaint, history_str, tags_ai_str, tags_legacy_str, level, extra_bin)
-                Console.log("received sql_data:")
-                Console.log(sql_data)
 
-                self.cur.execute(
-                    "INSERT INTO tickets (ticket_id, thread_id, name, customer_mail, complaint, history, tags_ai, tags_legacy, level, extra_bin) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                    sql_data)
-                self.con.commit()
-                Console.log("added to db")
+            Console.log("l60")
+            sql_data = (ticket_id, thread_id, name, customer_mail, complaint,
+                        history_str, ai_details, tags_legacy_str, level, extra_bin)
+            Console.log("received sql_data:")
+            Console.log(sql_data)
+
+            self.cur.execute(
+                "INSERT INTO tickets (ticket_id, thread_id, name, customer_mail, complaint, history, tags_ai, tags_legacy, level, extra_bin) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                sql_data)
+            self.con.commit()
+            Console.log("added to db")
         except Exception as e:
             Console.log("no ai tags recognised:")
             Console.log(e)
             tags_ai_str = "none"
             sql_data = (
-            ticket_id, thread_id, name, customer_mail, complaint, history_str, tags_ai_str, tags_legacy_str, level,
-            extra_bin)
+                ticket_id, thread_id, name, customer_mail, complaint, history_str, tags_ai_str, tags_legacy_str, level,
+                extra_bin)
 
             self.cur.execute(
                 "INSERT INTO tickets (ticket_id, thread_id, name, customer_mail, complaint, history, tags_ai, tags_legacy, level, extra_bin) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
@@ -84,11 +75,9 @@ class Ticket_db():
             self.con.commit()
             Console.log("added to db")
 
-
-
-        #res = self.cur.execute("SELECT * FROM tickets")
-        #rows = res.fetchall()
-        #print(rows)
+        # res = self.cur.execute("SELECT * FROM tickets")
+        # rows = res.fetchall()
+        # print(rows)
 
     def escalate(self):
         pass
@@ -98,5 +87,3 @@ class Ticket_db():
         pass
 
     # deleteticket, getforticket > tags, bin
-
-
--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 from test_cases.tests import input_tests
 from test_cases.test_mail import test_mail
 
-for i in range(10):
-    test_mail('erikblunk42@gmail.com',
-              'eeeeeeeeeeeeeeeeeeeeeeeeeeeee, eeee eee eeeeeeeeeee e eeeeeeee eeee eee eeeeeeeee ee eeeeee\neee e e eee')
+test_mail('erikblunk42@gmail.com',
+          'I need help with my lawn mower, its not connecting to my wifi network, I already looked at the faq but nothing I tried worked')
